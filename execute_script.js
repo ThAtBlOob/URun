fc10c6552071c13151e766bc21b7d0c6a1bb85a5
ThAtBlOob <143834475+ThAtBlOob@users.noreply.github.com>
2025-01-18 08:41:38 -0800

    Update execute_script.js

--- execute_script.js
+++ execute_script.js
@@ -1,2 +1,2 @@
 // execute_script.js
-window.addEventListener("keyup", event => { if (event.ctrlKey && event.which === 192) { eval(prompt("js to execute:")); } })
+document.addEventListener("keyup", event => { if (event.ctrlKey && event.which === 192) { eval(prompt("js to execute:")); } })
